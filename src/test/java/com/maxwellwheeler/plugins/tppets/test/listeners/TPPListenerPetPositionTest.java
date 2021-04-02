edaf1b093598aae2c3fba6c50ed97e69f6b66ae3
PenToPaper <wheelm11@gmail.com>
2021-04-02 18:37:47 -0400

    New SQLWrapper syntax to TPPListenerPetPositionTest

--- src/test/java/com/maxwellwheeler/plugins/tppets/test/listeners/TPPListenerPetPositionTest.java
+++ src/test/java/com/maxwellwheeler/plugins/tppets/test/listeners/TPPListenerPetPositionTest.java
@@ -3,7 +3,7 @@ package com.maxwellwheeler.plugins.tppets.test.listeners;
 import com.maxwellwheeler.plugins.tppets.TPPets;
 import com.maxwellwheeler.plugins.tppets.helpers.LogWrapper;
 import com.maxwellwheeler.plugins.tppets.listeners.ListenerPetPosition;
-import com.maxwellwheeler.plugins.tppets.storage.DBWrapper;
+import com.maxwellwheeler.plugins.tppets.storage.SQLWrapper;
 import com.maxwellwheeler.plugins.tppets.test.MockFactory;
 import org.bukkit.Chunk;
 import org.bukkit.OfflinePlayer;
@@ -17,16 +17,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class TPPListenerPetPositionTest {
-    private Chunk chunk;
     private Entity[] entities;
-    private TPPets tpPets;
-    private DBWrapper dbWrapper;
+    private SQLWrapper sqlWrapper;
     private ArgumentCaptor<Entity> entityCaptor;
     private ChunkUnloadEvent chunkUnloadEvent;
     private ListenerPetPosition listenerPetPosition;
@@ -34,30 +33,30 @@ public class TPPListenerPetPositionTest {
     @BeforeEach
     public void beforeEach() {
         OfflinePlayer petOwner = mock(OfflinePlayer.class);
-        this.chunk = mock(Chunk.class);
+        Chunk chunk = mock(Chunk.class);
         Horse entity1 = MockFactory.getTamedMockEntity("MockEntityId1", Horse.class, petOwner);
         Wolf entity2 = MockFactory.getTamedMockEntity("MockEntityId2", Wolf.class, petOwner);
         Villager entity3 = MockFactory.getMockEntity("MockEntityId3", Villager.class);
         this.entities = new Entity[]{entity1, entity2, entity3};
         this.entityCaptor = ArgumentCaptor.forClass(Entity.class);
-        this.dbWrapper = mock(DBWrapper.class);
+        this.sqlWrapper = mock(SQLWrapper.class);
 
         LogWrapper logWrapper = mock(LogWrapper.class);
-        this.tpPets = MockFactory.getMockPlugin(this.dbWrapper, logWrapper, true, false, true);
+        TPPets tpPets = MockFactory.getMockPlugin(this.sqlWrapper, logWrapper, true, false, true);
 
-        this.listenerPetPosition = new ListenerPetPosition(this.tpPets);
+        this.listenerPetPosition = new ListenerPetPosition(tpPets);
         this.chunkUnloadEvent = mock(ChunkUnloadEvent.class);
 
-        when(this.chunk.getEntities()).thenReturn(this.entities);
-        when(this.chunkUnloadEvent.getChunk()).thenReturn(this.chunk);
+        when(chunk.getEntities()).thenReturn(this.entities);
+        when(this.chunkUnloadEvent.getChunk()).thenReturn(chunk);
     }
 
     @Test
     @DisplayName("Registers pet positions in the database on chunk unload events")
-    void listsSpecificLostAndFoundRegions() {
+    void listsSpecificLostAndFoundRegions() throws SQLException {
         this.listenerPetPosition.onChunkUnload(this.chunkUnloadEvent);
 
-        verify(this.dbWrapper, times(2)).updateOrInsertPet(this.entityCaptor.capture());
+        verify(this.sqlWrapper, times(2)).insertOrUpdatePetLocation(this.entityCaptor.capture());
         List<Entity> entitiesUpdated = this.entityCaptor.getAllValues();
         assertEquals(this.entities[0], entitiesUpdated.get(0));
         assertEquals(this.entities[1], entitiesUpdated.get(1));
