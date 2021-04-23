31dcd5bb0fddace97307275972daaa34d340ef8b
PenToPaper <wheelm11@gmail.com>
2021-04-23 17:14:28 -0400

    Added TPPSQLWrapperInsertAllowedPlayerTest

---
+++ src/test/java/com/maxwellwheeler/plugins/tppets/test/storage/TPPSQLWrapperInsertAllowedPlayerTest.java
@@ -0,0 +1,89 @@
+package com.maxwellwheeler.plugins.tppets.test.storage;
+
+import com.maxwellwheeler.plugins.tppets.TPPets;
+import com.maxwellwheeler.plugins.tppets.helpers.LogWrapper;
+import com.maxwellwheeler.plugins.tppets.storage.SQLWrapper;
+import com.maxwellwheeler.plugins.tppets.test.MockFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class TPPSQLWrapperInsertAllowedPlayerTest {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private MockSQLWrapper mockSQLWrapper;
+    private ArgumentCaptor<Integer> indexCaptor;
+    private ArgumentCaptor<String> stringCaptor;
+
+    @BeforeEach
+    public void beforeEach() throws SQLException {
+        SQLWrapper sqlWrapper = mock(SQLWrapper.class);
+        LogWrapper logWrapper = mock(LogWrapper.class);
+        TPPets tpPets = MockFactory.getMockPlugin(sqlWrapper, logWrapper, false, false, false);
+        this.connection = mock(Connection.class);
+        this.preparedStatement = mock(PreparedStatement.class);
+
+        this.indexCaptor = ArgumentCaptor.forClass(Integer.class);
+        this.stringCaptor = ArgumentCaptor.forClass(String.class);
+
+        this.mockSQLWrapper = new MockSQLWrapper(tpPets, this.connection);
+
+        when(this.connection.prepareStatement("INSERT INTO tpp_allowed_players (pet_id, user_id) VALUES (?, ?)")).thenReturn(this.preparedStatement);
+        when(this.preparedStatement.executeUpdate()).thenReturn(1);
+    }
+
+    @Test
+    @DisplayName("insertAllowedPlayer returns true")
+    void insertAllowedPlayerReturnsTrue() throws SQLException {
+        assertTrue(this.mockSQLWrapper.insertAllowedPlayer("Mock-Pet-Id", "Mock-Player-Id"));
+
+        verify(this.preparedStatement, times(2)).setString(this.indexCaptor.capture(), this.stringCaptor.capture());
+        List<String> values = this.stringCaptor.getAllValues();
+        List<Integer> indexes = this.indexCaptor.getAllValues();
+
+        assertEquals(1, indexes.get(0));
+        assertEquals("MockPetId", values.get(0));
+
+        assertEquals(2, indexes.get(1));
+        assertEquals("MockPlayerId", values.get(1));
+
+        verify(this.preparedStatement, times(1)).executeUpdate();
+        verify(this.preparedStatement, times(1)).close();
+        verify(this.connection, times(1)).close();
+    }
+
+    @Test
+    @DisplayName("insertAllowedPlayer returns false if no rows inserted")
+    void insertAllowedPlayerReturnsFalse() throws SQLException {
+        when(this.preparedStatement.executeUpdate()).thenReturn(0);
+
+        assertFalse(this.mockSQLWrapper.insertAllowedPlayer("Mock-Pet-Id", "Mock-Player-Id"));
+
+        verify(this.preparedStatement, times(2)).setString(anyInt(), anyString());
+        verify(this.preparedStatement, times(1)).executeUpdate();
+        verify(this.preparedStatement, times(1)).close();
+        verify(this.connection, times(1)).close();
+    }
+
+    @Test
+    @DisplayName("insertAllowedPlayer rethrows exceptions")
+    void insertAllowedPlayerRethrowsExceptions() throws SQLException {
+        when(this.preparedStatement.executeUpdate()).thenThrow(new SQLException());
+
+        assertThrows(SQLException.class, () -> this.mockSQLWrapper.insertAllowedPlayer("Mock-Pet-Id", "Mock-Player-Id"));
+
+        verify(this.preparedStatement, times(2)).setString(anyInt(), anyString());
+        verify(this.preparedStatement, times(1)).executeUpdate();
+        verify(this.preparedStatement, times(1)).close();
+        verify(this.connection, times(1)).close();
+    }
+}
