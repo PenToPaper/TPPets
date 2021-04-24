7daa086b9687076bfbc0ab7863d8cc30b4c5c8e7
PenToPaper <wheelm11@gmail.com>
2021-04-23 23:34:00 -0400

    Added TPPSQLWrapperRemoveStorageLocationTest

---
+++ src/test/java/com/maxwellwheeler/plugins/tppets/test/storage/TPPSQLWrapperRemoveStorageLocationTest.java
@@ -0,0 +1,89 @@
+package com.maxwellwheeler.plugins.tppets.test.storage;
+
+import com.maxwellwheeler.plugins.tppets.TPPets;
+import com.maxwellwheeler.plugins.tppets.helpers.LogWrapper;
+import com.maxwellwheeler.plugins.tppets.storage.SQLWrapper;
+import com.maxwellwheeler.plugins.tppets.test.MockFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class TPPSQLWrapperRemoveStorageLocationTest {
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private MockSQLWrapper mockSQLWrapper;
+    private ArgumentCaptor<Integer> stringIndexCaptor;
+    private ArgumentCaptor<String> stringCaptor;
+
+    @BeforeEach
+    public void beforeEach() throws SQLException {
+        SQLWrapper sqlWrapper = mock(SQLWrapper.class);
+        LogWrapper logWrapper = mock(LogWrapper.class);
+        TPPets tpPets = MockFactory.getMockPlugin(sqlWrapper, logWrapper, false, false, false);
+        this.connection = mock(Connection.class);
+        this.preparedStatement = mock(PreparedStatement.class);
+
+        this.stringIndexCaptor = ArgumentCaptor.forClass(Integer.class);
+        this.stringCaptor = ArgumentCaptor.forClass(String.class);
+
+        this.mockSQLWrapper = new MockSQLWrapper(tpPets, this.connection);
+
+        when(this.connection.prepareStatement("DELETE FROM tpp_user_storage_locations WHERE user_id = ? AND effective_storage_name = ?")).thenReturn(this.preparedStatement);
+        when(this.preparedStatement.executeUpdate()).thenReturn(0);
+    }
+
+    @Test
+    @DisplayName("removeStorageLocation returns true")
+    void removeStorageLocationReturnsTrue() throws SQLException {
+        assertTrue(this.mockSQLWrapper.removeStorageLocation("Mock-Owner-Id", "MockStorageName"));
+
+        verify(this.preparedStatement, times(2)).setString(this.stringIndexCaptor.capture(), this.stringCaptor.capture());
+        List<Integer> stringIndexes = this.stringIndexCaptor.getAllValues();
+        List<String> strings = this.stringCaptor.getAllValues();
+
+        assertEquals(1, stringIndexes.get(0));
+        assertEquals("MockOwnerId", strings.get(0));
+
+        assertEquals(2, stringIndexes.get(1));
+        assertEquals("mockstoragename", strings.get(1));
+
+        verify(this.preparedStatement, times(1)).executeUpdate();
+        verify(this.preparedStatement, times(1)).close();
+        verify(this.connection, times(1)).close();
+    }
+
+    @Test
+    @DisplayName("removeLostRegion returns false when negative result from update")
+    void removeStorageLocationReturnsFalse() throws SQLException {
+        when(this.preparedStatement.executeUpdate()).thenReturn(-1);
+
+        assertFalse(this.mockSQLWrapper.removeStorageLocation("Mock-Owner-Id", "MockStorageName"));
+
+        verify(this.preparedStatement, times(2)).setString(anyInt(), anyString());
+        verify(this.preparedStatement, times(1)).executeUpdate();
+        verify(this.preparedStatement, times(1)).close();
+        verify(this.connection, times(1)).close();
+    }
+
+    @Test
+    @DisplayName("removeLostRegion rethrows exceptions")
+    void removeLostRegionRethrowsExceptions() throws SQLException {
+        when(this.preparedStatement.executeUpdate()).thenThrow(new SQLException());
+
+        assertThrows(SQLException.class, () -> this.mockSQLWrapper.removeStorageLocation("Mock-Owner-Id", "MockStorageName"));
+
+        verify(this.preparedStatement, times(2)).setString(anyInt(), anyString());
+        verify(this.preparedStatement, times(1)).executeUpdate();
+        verify(this.preparedStatement, times(1)).close();
+        verify(this.connection, times(1)).close();
+    }
+}
